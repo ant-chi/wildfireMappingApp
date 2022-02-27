6fc675dada8a3eded155726cae6bf6aa7e04d0d8
Chi <anchi@ucsd.edu>
2022-02-27 09:08:56 -0800

    etc from dropbox

--- funcs.py
+++ funcs.py
@@ -44,12 +44,14 @@ def loadModels():
 
     models["Logistic Regression"] = pickle.load(open("models/logistic_regression.sav", 'rb'))
     models["Multi-Layer Perceptron"] = pickle.load(open("models/mlp.sav", 'rb'))
+    models["Random Forest"] = joblib.load(open("models/rf.pkl", 'rb'))
+    # models["ETC 2"] = joblib.load(open("models/etc.pkl", 'rb'))
 
-    # models["RF OG"] = joblib.load(open("models/randomForest.joblib", 'rb'))
-    # models["ETC OG"] = joblib.load(open("models/extraTrees.joblib", 'rb'))
-
-    models["RF 2"] = joblib.load(open("models/rf.pkl", 'rb'))
-    models["ETC 2"] = joblib.load(open("models/etc.pkl", 'rb'))
+    etcURL = "https://www.dl.dropboxusercontent.com/s/jr8vwvz1tsee1f9/etc.pkl?dl=0"
+    etcRequest = requests.get(etcURL, allow_redirects=True)
+    open("models/etc.pkl", "wb").write(etcRequest.content)
+    del url
+    models["ETC dropbox"] = joblib.load(open("models/etc.pkl", 'rb'))
 
     models["SVM"] = joblib.load(open("models/svc.pkl", "rb"))
     models["log_boost"] = joblib.load(open("models/log_boost.pkl", "rb"))
